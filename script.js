8c8fa19086629c66ce7c8b1fdc78af8d44d0d5e8
Carlos Ledoux <carlosledoux@gmail.com>
2026-02-10 15:54:30 +0900

    Testing Live Site, Normal Page working

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-
-// const observer = new IntersectionObserver((cards) => {
-//     cards.forEach(card => {
-//         console.log(card);
-//         !card.isIntersecting ? card.target.classList.add("hidden") : card.target.classList.remove("hidden");
-//     });
-// });
-// document.querySelectorAll(".card-purple").forEach((card) => {
-//     observer.observe(card);
-// });
+const ninetiesBtn = document.getElementById("90s");
+const normalBtn = document.getElementById("normal");
+const fancyBtn = document.getElementById("fancy");
+const pageContent = document.getElementById("page-content");
 
 const boundaries = Array.from(document.querySelectorAll(".boundary"));
 console.log(boundaries);
@@ -29,3 +23,19 @@ const observer = new IntersectionObserver((cards) => {
 document.querySelectorAll(".boundary").forEach((card) => {
     observer.observe(card);
 });
+
+const changeHTML = (event, type) => {
+    event.preventDefault();
+    document.querySelectorAll(".fa-regular").forEach(btn => btn.classList.remove("selected"));
+    event.target.classList.add("selected");
+    fetch(`${type}.html`)
+        .then(response => response.text())
+        .then(html => {
+            console.log(html);
+            pageContent.innerHTML = html;
+        });
+}
+
+ninetiesBtn.addEventListener("click", event => changeHTML(event, "90s"));
+normalBtn.addEventListener("click", event => changeHTML(event, "normal"));
+fancyBtn.addEventListener("click", event => changeHTML(event, "fancy"));
