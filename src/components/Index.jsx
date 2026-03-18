d08f93703c3eafcf010038f5edbb116f6edbd4b0
Carlos Ledoux <carlosledoux@gmail.com>
2026-03-18 11:45:56 +0900

    Set up Framework

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,3 +1,4 @@
 export { default as Header } from "./layout/Header.jsx";
 export { default as Footer } from "./layout/Footer.jsx";
 export { default as About } from "./About.jsx";
+export { default as Projects } from "./Projects.jsx";
